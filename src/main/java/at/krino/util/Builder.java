416ced3b4a8141742679285bf958b4bcee16ad79
Jan van Oort <jan.vanoort@neotel.at>
2020-01-27 16:38:57 +0100

    Added parameterized Builder interface. Signed-off-by:Jan van Oort <jan.vanoort@neotel.at>

---
+++ src/main/java/at/krino/util/Builder.java
@@ -0,0 +1,17 @@
+/*
+ * krino: Copyright Jan van Oort, 2019
+ * 
+ */
+package at.krino.util;
+
+/**
+ *
+ * @author jan
+ */
+public interface Builder<T> {
+    
+    
+    public T build();
+    
+    
+}
