b9bba505cff221f9478bd0f4e268d5d5adc39df6
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2020-04-06 09:25:43 +0200

    Sememes updated in Morpheme.

--- src/Krino/Krino.Domain.ConstructiveAdpositionalGrammar/Morphemes/Morpheme.cs
+++ src/Krino/Krino.Domain.ConstructiveAdpositionalGrammar/Morphemes/Morpheme.cs
@@ -1,18 +1,19 @@
 ﻿using Krino.Vertical.Utils.Collections;
+using System.Collections.Generic;
 
 namespace Krino.Domain.ConstructiveAdpositionalGrammar.Morphemes
 {
     public class Morpheme
     {
-        public Morpheme (Morph morph, ITree<Sememe> sememes)
+        public Morpheme (Morph morph, List<ISememe> sememes)
         {
             Morph = morph;
-            Sememes = sememes;
+            Sememes = sememes ?? new List<ISememe>();
         }
 
         public Morph Morph { get; private set; }
 
-        public ITree<Sememe> Sememes { get; private set; }
+        public List<ISememe> Sememes { get; private set; }
 
 
     }
