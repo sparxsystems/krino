1814ac6c746c275355a63a5278247c7205d59e00
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2021-10-29 20:22:20 +0200

    Initial krino prolog service added.

---
+++ src/Krino/Krino.KnowledgeBase/Krino.pl
@@ -0,0 +1,68 @@
+:- use_module(library(apply)).
+
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/html_write)).
+
+:- dynamic clause/1.
+
+:- initialization(server(8123)).
+server(Port) :- http_server(http_dispatch, [port(Port)]).
+
+:- http_handler(root(add), handle_add, [post]).
+:- http_handler(root(add_multiple), handle_add_multiple, [post]).
+:- http_handler(root(remove), handle_remove, [delete]).
+:- http_handler(root(clear), handle_clear, []).
+:- http_handler(root(list), handle_list, [get]).
+:- http_handler(root(evaluate), handle_evaluate, [get]).
+
+
+handle_add(_) :-
+    reply_html_page(
+        [title('list')],
+        [h1('list')]
+    ).
+
+handle_add_multiple(_) :-
+    reply_html_page(
+        [title('list')],
+        [h1('list')]
+    ).
+
+handle_remove(_) :-
+    reply_html_page(
+        [title('list')],
+        [h1('list')]
+    ).
+
+handle_clear(_) :-
+    reply_html_page(
+        [title('list')],
+        [h1('list')]
+    ).
+
+handle_list(_) :-
+    reply_html_page(
+        [title('list')],
+        [h1('list')]
+    ).
+
+handle_evaluate(_) :-
+    reply_html_page(
+        [title('list')],
+        [h1('list')]
+    ).
+
+
+
+add(Statement) :- remove(Statement), assertz(clause(Statement)).
+add_multiple(Statements) :- maplist(add, Statements).
+
+remove(Statement) :- retractall(clause(Statement)).
+clear :- retractall(clause(_)).
+
+list :- listing(clause/1).
+
+evaluate(Statement) :- clause(Statement).
+
