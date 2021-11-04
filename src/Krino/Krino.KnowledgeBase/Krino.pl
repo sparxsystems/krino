d1b2f829d47c67faf528eec3fad50880e65fd39d
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2021-11-04 21:22:41 +0100

    Krino prolog in progress.

--- src/Krino/Krino.KnowledgeBase/Krino.pl
+++ src/Krino/Krino.KnowledgeBase/Krino.pl
@@ -10,7 +10,7 @@
 
 :- dynamic clause/1.
 
-:- initialization(server(8123)).
+%:- initialization(server(8123)).
 server(Port) :- http_server(http_dispatch, [port(Port)]).
 
 :- http_handler(root(add), handle_add, [method(post)]).
@@ -64,6 +64,73 @@ list :- listing(clause/1).
 
 evaluate(Statement) :- clause(Statement).
 
-    
 
+
+has(a, b).
+has(b, c).
+has(c, d).
+
+% Demaging the grass is prohibited.
+k_prohibited(Activity, Tracking) :- k_prohibited(Activity, [], Tracking).
+k_prohibited(Activity, PreviousTracking, NewTracking) :-
+    k_tracking_call(k_demages(Activity, grass), PreviousTracking, NewTracking).
+
+% Walking on the grass demages the grass.
+k_demages(k_on(walking, grass), grass).
+
+% Cycling on the grass demages the grass.
+k_demages(k_on(cycling, grass), grass).
+
+% Cycling on the grass is prohibited because walking on the grass is prohibited.
+% Form: a is X because b is X
+
+% He is loudly because he is deaf.
+% Form: a is X because a is Y
+
+
+
+
+k_argument(Conclusion, Premise, Lever) :-
+    k_tracking_call(Premise, [], PremiseTrackingResult),
+    k_tracking_call(Conclusion, [], ConclusionTrackingResult),
+    intersection(ConclusionTrackingResult, PremiseTrackingResult, Lever).
+
+
+
+relation(Goal, X, reflexive) :- call(Goal, X, X).
+
+relation(Goal, X, Y, symmetric) :-
+    call(Goal, X, Y),
+    call(Goal, Y, X).
+
+relation(Goal, X, Y, transitive, TrackingResult) :- relation(Goal, X, Y, [], transitive, [], TrackingResult).
+relation(Goal, X, Y, L, transitive, PreviousTracking, NewTracking) :-
+    Term =.. [Goal, X, Y],
+    k_tracking_call(Term, PreviousTracking, NewTracking),
+    \+ memberchk((X, Y), L).
+relation(Goal, X, Y, L, transitive, PreviousTracking, NewTracking) :-
+    Term =.. [Goal, X, Z],
+    k_tracking_call(Term, PreviousTracking, NewTrackingTmp),
+    \+ memberchk((X, Z), L),
+    relation(Goal, Z, Y, [(X, Z) | L], transitive, NewTrackingTmp, NewTracking).
+
+
+k_tracking_call(Goal, PreviousTracking, NewTracking) :-
+    % Get the goal which incudes parameteres for the tracking.
+    Goal =.. [GoalName | _],
+    functor(Goal, GoalName, GoalArity),
+    TrackableGoalArity is GoalArity + 2,
+    functor(TrackingGoal, GoalName, TrackableGoalArity),
+
+    % if the goal with the arity for the tracking exists.
+    current_predicate(_, TrackingGoal),
+    % call the goal with the tracking parameters
+    call(Goal, PreviousTracking, NewTracking)
+    ;
+    % if the goal exists.
+    current_predicate(_, Goal),
+    call(Goal),
+    append(PreviousTracking, [Goal], NewTracking).
+
+    
 
