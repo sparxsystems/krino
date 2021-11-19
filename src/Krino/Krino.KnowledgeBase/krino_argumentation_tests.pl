80b57d1db5e09e293585ad3ab612de4c16fa2fc7
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2021-11-19 11:00:46 +0100

    Knowledge base imroved, diagram updated.

--- src/Krino/Krino.KnowledgeBase/krino_argumentation_tests.pl
+++ src/Krino/Krino.KnowledgeBase/krino_argumentation_tests.pl
@@ -7,27 +7,28 @@ is_prohibited(Activity) :- damages(Activity, grass).
 % If an activity offends the grass then it is prohibited.
 is_prohibited(Activity) :- offends(Activity, grass).
 
-% If an activity touches the grass then it damages the grass.
-damages(Activity, grass) :- touches(Activity, grass).
+% If an activity moves the grass then it damages the grass.
+damages(Activity, grass) :- moves(Activity, grass).
 
-% If an activity moves the grass then it touches the grass.
-touches(Activity, grass) :- moves(Activity, grass).
+% If an activity touches the grass then it moves the grass.
+moves(Activity, grass) :- touches(Activity, grass).
 
-% Walking on the grass moves the grass.
-moves(on(walking, grass), grass).
+% Walking on the grass touches the grass.
+touches(on(walking, grass), grass).
 
-% Cycling on the grass moves the grass.
-moves(on(cycling, grass), grass).
+% Cycling on the grass touches the grass.
+touches(on(cycling, grass), grass).
 
 % Gazing on the grass offends the grass.
 offends(on(gazing, grass), grass).
+
 
 is_verb(is_prohibited).
 is_verb(damages).
 is_verb(touches).
 is_verb(moves).
 is_verb(offends).
-is_verb(has).
+
 
 
 has(a).
@@ -78,21 +79,21 @@ test(k_argument_evaluate_aXbX) :-
 test(k_argument_evaluate_aXbX_no_lever) :-
     \+ k_argument_evaluate(because(is_prohibited(on(cycling, grass)), is_prohibited(on(gazing, grass))), _), !.
 
-% Cycling on the grass is prohibited because walking on the grass touches the grass.
+% Cycling on the grass is prohibited because walking on the grass moves the grass.
 % Incorrect argument because there is no a common term (fulcrum) between the conclusion and the premise.
 test(k_argument_evaluate_no_form) :-
-    \+ k_argument_evaluate(because(is_prohibited(on(cycling, grass)), touches(on(walking, grass), grass)), _), !.
+    \+ k_argument_evaluate(because(is_prohibited(on(cycling, grass)), moves(on(walking, grass), grass)), _), !.
 
-% Walking on the grass damages the grass because it touches the grass.
+% Walking on the grass damages the grass because it moves the grass.
 test(k_argument_evaluate_aXaY) :-
-    k_argument_evaluate(because(damages(on(walking, grass), grass), touches(on(walking, grass), grass)), Lever), !,
+    k_argument_evaluate(because(damages(on(walking, grass), grass), moves(on(walking, grass), grass)), Lever), !,
     %write(["Lever: " | Lever]),
-    Lever = touches(on(walking, grass), grass).
+    Lever = moves(on(walking, grass), grass).
 
-% Walking on the grass is prohibited because it touches the grass.
+% Walking on the grass is prohibited because it moves the grass.
 % Incorrect argument because the prohibition is caused by demaging and not touching.
 test(k_argument_evaluate_aXaY_unrelated_predicates) :-
-    \+ k_argument_evaluate(because(is_prohibited(on(walking, grass)), touches(on(walking, grass), grass)), _), !.
+    \+ k_argument_evaluate(because(is_prohibited(on(walking, grass)), moves(on(walking, grass), grass)), _), !.
 
 test(k_argument_evaluate_no_lever) :-
     \+ k_argument_evaluate(because(has(a, b), has(a, c)), _).
