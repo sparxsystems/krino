b973beb7d032588af8904e4155c7a4a354e54e36
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2022-09-25 19:55:44 +0200

    Parsing of verbs improved.

--- src/Krino/Krino.ConstructiveGrammar/LinguisticStructures/Attributes/AuxiliaryVerbAttributes.cs
+++ src/Krino/Krino.ConstructiveGrammar/LinguisticStructures/Attributes/AuxiliaryVerbAttributes.cs
@@ -10,8 +10,14 @@ namespace Krino.ConstructiveGrammar.LinguisticStructures.Attributes
             Modal = new EnumValue(this);
         }
 
+        /// <summary>
+        /// The primary auxiliary verbs are ‘be’, ‘have’ and ‘do’. These verbs modify other verbs in a full verb phrase, e.g. ‘is going’, ‘has gone’, or ‘did go’.
+        /// </summary>
         public EnumValue Primary { get; }
 
+        /// <summary>
+        /// Modal auxiliary verbs like ‘can’ and ‘should’ usually occur with main verbs, e.g. ‘can pay’, ‘should pay’. They add meanings like possibility and obligation to the main verb.
+        /// </summary>
         public EnumValue Modal { get; }
     }
 }
--- src/Krino/Krino.KnowledgeBase/krino_english_grammar.pl
+++ src/Krino/Krino.KnowledgeBase/krino_english_grammar.pl
@@ -5,7 +5,7 @@
 
 % Eliminate the left recusion infinite loop.
 :- table sentence/3.
-:- table clause/3.
+:- table clauze/3.
 :- table subject/3.
 :- table predicate/3.
 :- table noun_phrase/3.
@@ -13,14 +13,18 @@
 :- table adverb_phrase/3.
 
 
+text(Text) --> sentences(Sentences), { Text = text(Sentences) }.
+
 sentence(Sentence) --> independent_clauses(Clauses), punctuation_mark(PunctuationMark), { Sentence = sentence(Clauses, PunctuationMark) }.
+sentences([Sentence]) --> sentence(Sentence).
+sentences(Sentences) --> sentence(Sentence), sentences(NextSentences), { append([Sentence], NextSentences, Sentences) }.
 
-clause(Clause) --> subject(Subject), predicate(Predicate), { Clause = clause(independent, Subject, Predicate)}.
-clause(Clause) --> coordinating_conjunction(Conjunction), subject(Subject), predicate(Predicate), { Clause = clause(independent, Conjunction, Subject, Predicate)}.
-clause(Clause) --> subordinating_conjunction(Conjunction), subject(Subject), predicate(Predicate), { Clause = clause(dependent, Conjunction, Subject, Predicate)}.
+clauze(Clause) --> subject(Subject), predicate(Predicate), { Clause = clauze(independent, Subject, Predicate)}.
+clauze(Clause) --> coordinating_conjunction(Conjunction), subject(Subject), predicate(Predicate), { Clause = clauze(independent, Conjunction, Subject, Predicate)}.
+clauze(Clause) --> subordinating_conjunction(Conjunction), subject(Subject), predicate(Predicate), { Clause = clauze(dependent, Conjunction, Subject, Predicate)}.
 
-independent_clause(Clause) --> clause(Clause), { Clause = clause(independent, _, _, _) ;  Clause = clause(independent, _, _) }.
-dependent_clause(Clause) --> clause(Clause), { Clause = clause(dependent, _, _, _) }.
+independent_clause(Clause) --> clauze(Clause), { Clause = clauze(independent, _, _, _) ;  Clause = clauze(independent, _, _) }.
+dependent_clause(Clause) --> clauze(Clause), { Clause = clauze(dependent, _, _, _) }.
 
 independent_clauses([Clause]) --> independent_clause(Clause).
 independent_clauses(Clauses) --> independent_clause(Clause), independent_clauses(NextClauses), { append([Clause], NextClauses, Clauses) }.
@@ -44,8 +48,14 @@ noun_phrase(NounPhrase) --> determiner(Determiner), adjective_phrase(Attributive
 
 noun_phrase(NounPhrase) --> noun_phrase(Phrase1), coordinating_conjunction(Conjunction), noun_phrase(Phrase2), { NounPhrase = noun_phrase(conjunction(Conjunction, Phrase1, Phrase2)) }.
 
+verb_phrase(VerbPhrase) --> present_simple(Verb), { VerbPhrase = verb_phrase(Verb) }.
 
-verb_phrase(VerbPhrase) --> present_simple(VerbPhrase).
+present_simple(PresentSimple) --> present_verb(Verb), { PresentSimple = present_simple(Verb) }.
+present_simple(PresentSimple) --> verb(AuxVerb), present_verb(Verb),
+    { 
+        (AuxVerb = verb(auxiliary(primary(_))) ; AuxVerb = verb(auxiliary(modal(_)))),
+        PresentSimple = present_simple(AuxVerb, Verb)
+    }.
 
 
 adjective_phrase(AdjectivePhrase) --> adjective(AdjectivePhrase) ; cardinal(AdjectivePhrase) ; past_participle(AdjectivePhrase).
@@ -56,7 +66,7 @@ adverb_phrase(AdverbPhrase) --> adverb(AdverbPhrase).
 adverb_phrase(AdverbPhrase) --> adverb_phrase(Adverb1), adverb_phrase(Adverb2), { AdverbPhrase = adverb_phrase(Adverb1, Adverb2) }.
 adverb_phrase(AdverbPhrase) --> adverb_phrase(Phrase1), coordinating_conjunction(Conjunction), adverb_phrase(Phrase2), { AdverbPhrase = adverb_phrase(conjunction(Conjunction, Phrase1, Phrase2)) }.
 
-
+infinitive_phrase(InfinitivePhrase) --> infinitive_marker(To), present_verb(Verb), { InfinitivePhrase = infinitive_phrase(To, Verb) }.
 
 
 
@@ -65,12 +75,50 @@ adverb_phrase(AdverbPhrase) --> adverb_phrase(Phrase1), coordinating_conjunction
 % concatenates(Conjunction, Phrase1, Phrase2, ResultTerm) :- ResultTerm =.. [Conjunction, Phrase1, Phrase2].
 % infinitive(InfinitiveMarker, Verb, inf(InfinitiveMarker, Verb)).
 
+present_verb(Verb) --> verb(Verb), { Verb = verb(present(_)) }.
+
+
+regular_verb(Verb, Base, Past) :- Verb = verb(regular(Base)) ; all_verb_forms(Verb,  Base,  Base, Base, Base, Base, Base, Base,  Past, Past, Past, Past, Past, Past,  Past).
+irregular_tense_verb(Verb, Base, Past, PastParticple) :- Verb = verb(irregular(Base)) ; all_verb_forms(Verb,  Base,  Base, Base, Base, Base, Base, Base,  Past, Past, Past, Past, Past, Past,  PastParticple).
+
+all_verb_forms(VerbTerm, Base,
+               PresentSingularFirstPerson, PresentSingularSecondPerson, PresentSingularThirdPerson,
+               PresentPluralFirstPerson, PresentPluralSecondPerson, PresentPluralThirdPerson,
+               PastSingularFirstPerson, PastSingularSecondPerson, PastSingularThirdPerson,
+               PastPluralFirstPerson, PastPluralSecondPerson, PastPluralThirdPerson,
+               PastParticiple) :-
+    VerbTerm = verb(base(Base)) ;
+
+    VerbTerm = verb(present(singular(first_person(PresentSingularFirstPerson)))) ;
+    VerbTerm = verb(present(singular(second_person(PresentSingularSecondPerson)))) ;
+    VerbTerm = verb(present(singular(third_person(PresentSingularThirdPerson)))) ;
+    VerbTerm = verb(present(plural(first_person(PresentPluralFirstPerson)))) ;
+    VerbTerm = verb(present(plural(second_person(PresentPluralSecondPerson)))) ;
+    VerbTerm = verb(present(plural(third_person(PresentPluralThirdPerson)))) ;
+
+    VerbTerm = verb(past(singular(first_person(PastSingularFirstPerson)))) ;
+    VerbTerm = verb(past(singular(second_person(PastSingularSecondPerson)))) ;
+    VerbTerm = verb(past(singular(third_person(PastSingularThirdPerson)))) ;
+    VerbTerm = verb(past(plural(first_person(PastPluralFirstPerson)))) ;
+    VerbTerm = verb(past(plural(second_person(PastPluralSecondPerson)))) ;
+    VerbTerm = verb(past(plural(third_person(PastPluralThirdPerson)))) ;
+
+    VerbTerm = verb(past_participle(PastParticiple)).
+    
+
+
+
+
+verb(Verb) --> ['be'], { Verb = verb(irregular(be)) ; all_verb_forms(Verb, be,  am, are, 'is', are, are, are,  was, were, was, were, were, were,  been) }.
+verb(verb(linking('be'))) --> ['be'].
+
+verb(Verb) --> [do], { irregular_tense_verb(Verb, do, did, done) }.
+verb(verb(auxiliary(primary(do)))) --> [do].
+
 verb(verb(present_simple, read)) --> [read].
 verb(verb(present_simple, write)) --> [write].
-verb(verb(present_simple, is)) --> ['is'].
-verb(verb(linking, is)) --> ['is'].
 
-present_simple(Verb) --> verb(Verb), { Verb = verb(present_simple, _) }.
+
 
 
 determiner(the) --> [the].
